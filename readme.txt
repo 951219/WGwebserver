8508a24cbbaaf3619b068f8eff8df79f10bf0a76
Kenert Karu <karukenert@gmail.com>
2020-04-21 00:37:40 +0300

    initial commit

---
+++ readme.txt
@@ -0,0 +1,1 @@
+info siit: https://dev.to/lenmorld/quick-server-with-node-and-express-in-5-minutes-17m7
---
+++ server.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const server = express();
+const body_parser = require('body-parser')
+
+server.use(body_parser.json());
+
+// add json route handler
+server.get("/json", (req, res) => {
+    res.json({
+        message: "Hello world"
+    });
+});
+
+
+
+
+//add html route handler
+server.get("/", (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+server.get("/info", (req, res) => {
+    res.sendFile(__dirname + '/info.html');
+});
+
+//start server 
+
+const port = 4000;
+
+server.listen(port, () => {
+    console.log(`Server listening at ${port}`);
+});
+
+
+
+let data = require('./data')
+
+
+server.get("/items", (req, res) => {
+    res.json(data);
+});
+
+server.get("/items/:id", (req, res) => {
+    const itemId = req.params.id;
+    const item = data.find(_item => _item.id === itemId);
+
+    if (item) {
+        res.json(item);
+    } else {
+        res.json({
+            message: `item ${itemId} doesn't exist`
+        })
+    }
+});
+
+
+server.post("/items", (req, res) => {
+    const item = req.body;
+    console.log('Adding new item: ', item)
+
+    data.push(item);
+
+    res.json(data);
+})
+
+server.put("/items/:id", (req, res) => {
+    const itemId = req.params.id;
+    const item = req.body;
+    console.log("Editing item: ", itemId, " to be ", item);
+ 
+    const updatedListItems = [];
+    // loop through list to find and replace one item
+    data.forEach(oldItem => {
+       if (oldItem.id === itemId) {
+          updatedListItems.push(item);
+       } else {
+          updatedListItems.push(oldItem);
+       }
+    });
+ 
+    // replace old list with new one
+    data = updatedListItems;
+ 
+    res.json(data);
+ });
+
+ server.delete("/items/:id", (req, res) => {
+    const itemId = req.params.id;
+ 
+    console.log("Delete item with id: ", itemId);
+ 
+    const filtered_list = data.filter(item => item.id !== itemId);
+ 
+    data = filtered_list;
+ 
+    res.json(data);
+ });
