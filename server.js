d280cc67cad5ee846ba64e1e1048e2059270b421
Kenert Karu <karukenert@gmail.com>
2020-04-21 10:01:24 +0300

    working crud

--- server.js
+++ server.js
@@ -33,74 +33,72 @@ server.listen(port, () => {
 });
 
 
-
-let data = require('./data')
-
+// CRUD / REST / DISKDB
 
 server.get("/items", (req, res) => {
-    res.json(data);
+    res.json(db.movies.find());
 });
 
 server.get("/items/:id", (req, res) => {
     const itemId = req.params.id;
-    const item = data.find(_item => _item.id === itemId);
-
-    if (item) {
-        res.json(item);
+    const items = db.movies.find({ id: itemId });
+    if (items.length) {
+       res.json(items);
     } else {
-        res.json({
-            message: `item ${itemId} doesn't exist`
-        })
+       res.json({ message: `item ${itemId} doesn't exist` })
     }
-});
+ });
 
 
 server.post("/items", (req, res) => {
     const item = req.body;
     console.log('Adding new item: ', item)
 
-    data.push(item);
+    db.movies.save(item);
 
-    res.json(data);
+    res.json(db.movies.find());
 })
 
+
+//TODO put not working in postman - edit: id urli l6ppu
 server.put("/items/:id", (req, res) => {
     const itemId = req.params.id;
     const item = req.body;
     console.log("Editing item: ", itemId, " to be ", item);
  
-    const updatedListItems = [];
-    // loop through list to find and replace one item
-    data.forEach(oldItem => {
-       if (oldItem.id === itemId) {
-          updatedListItems.push(item);
-       } else {
-          updatedListItems.push(oldItem);
-       }
-    });
+    db.movies.update({ id: itemId }, item);
  
-    // replace old list with new one
-    data = updatedListItems;
- 
-    res.json(data);
+    res.json(db.movies.find({ id: itemId }));
  });
 
- server.delete("/items/:id", (req, res) => {
+ //works with "curl -X PUT -H "Content-Type: application/json" --data '{"name": "banana vaarikaga"}' http://localhost:4000/items/3"
+
+
+server.delete("/items/:id", (req, res) => {
     const itemId = req.params.id;
- 
     console.log("Delete item with id: ", itemId);
- 
-    const filtered_list = data.filter(item => item.id !== itemId);
- 
-    data = filtered_list;
-    res.json(data);
- });
 
- if (!db.movies.find().length) {
-    const movie = { id: "tt0110358", name: "The Tiger King", genre: "animation" };
+    db.movies.remove({
+        id: itemId
+    });
+
+    res.json(db.movies.find());
+});
+
+
+
+
+
+//testdata juhuks kui db tyhi
+if (!db.movies.find().length) {
+    const movie = {
+        id: "tt0110358",
+        name: "The Tiger King",
+        genre: "animation"
+    };
     db.movies.save(movie);
- }
- console.log(db.movies.find());
+}
+console.log(db.movies.find());
 
 
- //kuuenda juures pooleli. https://dev.to/lenmorld/quick-database-with-node-express-and-diskdb-in-5-minutes-1jjj
+//kuuenda juures pooleli. https://dev.to/lenmorld/quick-database-with-node-express-and-diskdb-in-5-minutes-1jjj
