f76eac3f81da99ae5049b1dcbfce7e2bec44da80
Kenert Karu <karukenert@gmail.com>
2020-12-29 14:29:56 +0200

    pulling from exilex finished

--- routes/est2.js
+++ routes/est2.js
@@ -5,13 +5,13 @@ const fetch = require('node-fetch');
 
 router.get('/ekilex/:word', async (req, res) => {
 
-    let word = await getWord(req.params.word);
-    let details = await getDefinitions(word);
+    let reqWord = await getWord(req.params.word);
+    let details = await getWordDetails(reqWord);
+    let completedWord = await createAWord(details);
 
-    res.json({
-        word: word,
-        details: details
-    })
+    res.json(
+        completedWord
+    );
 
 });
 
@@ -53,16 +53,16 @@ router.get('/ekilex/:word', async (req, res) => {
 // }
 
 
-
-//TODO pull word IDs from ekilex
 //TODO pull definitions with IDs from ekilex and create a word to return
 
+
+// 1. Getting the word id by word - https://ekilex.eki.ee/api/word/search/{word}/sss
 async function getWord(reqWord) {
-    const url = `https://ekilex.eki.ee/api/word/search/${reqWord}/sss`;
+    const url = `https://ekilex.eki.ee/api/word/search/${encodeURI(reqWord)}/sss`;
 
     let word;;
     try {
-        const fetch_response = await fetch(url, { method: 'GET', headers: { 'ekilex-api-key': process.env.EKILEX_API_KEY } });
+        const fetch_response = await fetch(url, { method: 'GET', headers: { 'ekilex-api-key': process.env.EKILEX_API_KEY } }).catch(err => console.error(err));
         const json = await fetch_response.json();
         word = json;
         console.log('Success - got the word from ekilex');
@@ -72,9 +72,8 @@ async function getWord(reqWord) {
     return word;
 }
 
-
-
-async function getDefinitions(word) {
+// 2. Getting the definitions by word id - https://ekilex.eki.ee/api/word/details/{wordID}
+async function getWordDetails(word) {
     const url = `https://ekilex.eki.ee/api/word/details/${word["words"][0]["wordId"]}`;
 
     let wordDetails;
@@ -89,20 +88,34 @@ async function getDefinitions(word) {
     return wordDetails;
 }
 
+// 3. Create a word object
+// a. "lexemes[]"
+// b. igaühel"meaning{}"
+// c. igal objectil  definitions[]
+// d. igal objectil value
+async function createAWord(wordDetails) {
+    let wordId = wordDetails["lexemes"][0]["wordId"];
+    let word = wordDetails["lexemes"][0]["wordValue"];
+    let definitions = [];
+    let examples = [];
+
+    let lexemes = wordDetails["lexemes"];
+
+    lexemes.forEach(element => {
+        element["meaning"]["definitions"].forEach(item => {
+            definitions.push(item["value"]);
+        })
+    });
+
+    lexemes.forEach(element => {
+        element["usages"].forEach(item => {
+            examples.push(item["value"]);
+        })
+    });
+
+    return { wordId, word, definitions, examples };
+}
+
 module.exports = router;
 
-/*
-Returns word id by Word from EKI API - https://ekilex.eki.ee/api/word/search/{word}/sss  - sss - EKI sõnastik 2020
-Returns definitions by word it from EKI API - https://ekilex.eki.ee/api/word/details/{wordID}
-ekilex-api-key as a header is necessary for both
-
-1. Getting the word id by word - https://ekilex.eki.ee/api/word/search/{word}/sss
-2. Getting the definitions by word id - https://ekilex.eki.ee/api/word/details/{wordID}
-    a. "lexemes[]"
-    b. igaühel"meaning{}"
-    c. igal objectil  definitions[]
-    d. igal objectil value
-3. Create a word object
-
-Getting the examples by word id ????
-*/
+// TODO getting the score and id from mongo
