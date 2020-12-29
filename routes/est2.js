7238d6775077ea347279c2d1123d1d2eb7f26646
Kenert Karu <karukenert@gmail.com>
2020-12-29 13:11:43 +0200

    /est2/ekilex/:word endpoint added, it returns word and meaning from exilex

--- routes/est2.js
+++ routes/est2.js
@@ -4,11 +4,14 @@ const fetch = require('node-fetch');
 
 
 router.get('/ekilex/:word', async (req, res) => {
-    const url = `https://ekilex.eki.ee/api/word/search/${req.params.word}/sss`;
-    console.log(url);
-    const fetch_response = await fetch(url, { method: 'GET', headers: { 'ekilex-api-key': process.env.EKILEX_API_KEY } });
-    const json = await fetch_response.json();
-    res.json(json);
+
+    let word = await getWord(req.params.word);
+    let details = await getDefinitions(word);
+
+    res.json({
+        word: word,
+        details: details
+    })
 
 });
 
@@ -54,17 +57,36 @@ router.get('/ekilex/:word', async (req, res) => {
 //TODO pull word IDs from ekilex
 //TODO pull definitions with IDs from ekilex and create a word to return
 
-async function getWord(req, res, next) {
-    let word;
+async function getWord(reqWord) {
+    const url = `https://ekilex.eki.ee/api/word/search/${reqWord}/sss`;
+
+    let word;;
     try {
-        // const fetch_response = await fetch(url, { method: 'GET', headers: { 'ekilex-api-key': process.env.EKILEX_API_KEY } });
-        // const json = await fetch_response.json();
-        // res.json(json);
+        const fetch_response = await fetch(url, { method: 'GET', headers: { 'ekilex-api-key': process.env.EKILEX_API_KEY } });
+        const json = await fetch_response.json();
+        word = json;
+        console.log('Success - got the word from ekilex');
     } catch (err) {
-        return res.status(500).json({ message: err.message });
+        return { message: err.message };
     }
-    res.word = word;
-    next();
+    return word;
+}
+
+
+
+async function getDefinitions(word) {
+    const url = `https://ekilex.eki.ee/api/word/details/${word["words"][0]["wordId"]}`;
+
+    let wordDetails;
+    try {
+        const fetch_response = await fetch(url, { method: 'GET', headers: { 'ekilex-api-key': process.env.EKILEX_API_KEY } });
+        const json = await fetch_response.json();
+        wordDetails = json;
+        console.log('Success - got the word details from ekilex');
+    } catch (err) {
+        return { message: err.message };
+    }
+    return wordDetails;
 }
 
 module.exports = router;
@@ -73,4 +95,14 @@ module.exports = router;
 Returns word id by Word from EKI API - https://ekilex.eki.ee/api/word/search/{word}/sss  - sss - EKI sõnastik 2020
 Returns definitions by word it from EKI API - https://ekilex.eki.ee/api/word/details/{wordID}
 ekilex-api-key as a header is necessary for both
+
+1. Getting the word id by word - https://ekilex.eki.ee/api/word/search/{word}/sss
+2. Getting the definitions by word id - https://ekilex.eki.ee/api/word/details/{wordID}
+    a. "lexemes[]"
+    b. igaühel"meaning{}"
+    c. igal objectil  definitions[]
+    d. igal objectil value
+3. Create a word object
+
+Getting the examples by word id ????
 */
